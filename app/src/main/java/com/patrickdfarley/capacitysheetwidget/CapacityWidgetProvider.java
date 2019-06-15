cd80f9da0eaa8a299ed9ad89a87995277cbad754
Patrick D Farley <patrickfarley.work@gmail.com>
2019-06-15 14:14:13 -0700

    do not start activity on widget onUpdate

--- app/src/main/java/com/patrickdfarley/capacitysheetwidget/CapacityWidgetProvider.java
+++ app/src/main/java/com/patrickdfarley/capacitysheetwidget/CapacityWidgetProvider.java
@@ -22,15 +22,12 @@ public class CapacityWidgetProvider extends AppWidgetProvider{
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             // Get the view for the App Widget layout
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.capacity_appwidget);
+            //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.capacity_appwidget);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            //appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 }
