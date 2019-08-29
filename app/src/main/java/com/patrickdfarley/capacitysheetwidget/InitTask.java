92c0c8b7d5131601ffdffc29501e5f0158c62ad1
PatrickFarley <patrickfarley.work@gmail.com>
2019-08-28 19:06:45 -0700

    task needs to handle the case of Google Sheet perms not granted to the app.

--- app/src/main/java/com/patrickdfarley/capacitysheetwidget/InitTask.java
+++ app/src/main/java/com/patrickdfarley/capacitysheetwidget/InitTask.java
@@ -1,8 +1,10 @@
 package com.patrickdfarley.capacitysheetwidget;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
@@ -11,6 +13,7 @@ import android.widget.RemoteViews;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -26,10 +29,14 @@ public class InitTask extends AsyncTask<Void, Void, List<List<Object>>> {
     private com.google.api.services.sheets.v4.Sheets sheetsService;
     private Exception mLastError = null;
     private static final String TAG = "InitTask";
+    private static final int OFFSETTOP = 3;
+    private static final int OFFSETBOTTOM = 2;
+
 
     private ProgressDialog mProgress;
     private Context context;
     private SharedPreferences sharedPreferences;
+    private int categoryCount;
 
     public AppWidgetManager appWidgetManager;
     public int appWidgetID;
@@ -99,51 +106,84 @@ public class InitTask extends AsyncTask<Void, Void, List<List<Object>>> {
         String sheetName = sharedPreferences.getString("SheetName","");
         String dataRange = sharedPreferences.getString("DataRange","");
 
+        // convert to points (row,col)
+        Point[] dataRangePoints = A12Coords(dataRange);
+        // record category count
+        categoryCount = getRowCount(dataRangePoints);
         // Manually expand the range here: we want the first rows too.
-        final String finalRange = "'" + sheetName + "'!" + dataRange.replaceFirst("\\d+","1");;
-        Log.d(TAG,finalRange);
+        dataRangePoints[0].x -= OFFSETTOP;
+        dataRangePoints[1].x += OFFSETBOTTOM;
 
+        // create A1 string for use in Sheets API lookup
+        final String finalRange = "'" + sheetName + "'!" + Coords2A1(dataRangePoints);
+        Log.d(TAG,"finalRange: " + finalRange);
+        Log.d(TAG,"sheet id: " + spreadsheetId);
         ValueRange response = this.sheetsService.spreadsheets().values()
                 .get(spreadsheetId, finalRange)
                 .execute();
         List<List<Object>> responseData = response.getValues();
 
         // TODO check for successful response and handle unsuccessful
+        Log.d(TAG,"responsedata:" + responseData);
         return responseData;
     }
 
     // runs in the UI thread: update UI
     @Override
-    protected void onPostExecute(List<List<Object>> output) {
+    protected void onPostExecute(List<List<Object>> responseData) {
         mProgress.hide();
-        if (output == null || output.size() == 0) {
+        if (responseData == null || responseData.size() == 0) {
             Log.d(TAG, "No results returned.");
         } else {
-            Log.d(TAG, TextUtils.join("\n", output));
+            Log.d(TAG, TextUtils.join("\n", responseData));
 
             // create an updated RemoteViews
             RemoteViews newView = remoteViews;
-            int currentWeekIndex = getCurrentWeekIndex(output);
-
-            // figure out how many category rows to add:
-            //String dataRange = sharedPreferences.getString("DataRange","");
-            int categoryCount = getRowCount(output);
+            int currentWeekIndex = getCurrentWeekIndex(responseData);
 
             // add category amounts:
-            int offset = 3; //TODO: this should be saved more carefully
-            for(int i=offset;i<categoryCount+offset;i++){
+            for(int i = OFFSETTOP; i<categoryCount+ OFFSETTOP; i++){
                 RemoteViews childView = new RemoteViews(context.getPackageName(),R.layout.category_item);
-                String catAmount = output.get(i).size()>currentWeekIndex ? (String) (output.get(i).get(currentWeekIndex)) : "";
+                String catAmount = responseData.get(i).size()>currentWeekIndex ? (String) (responseData.get(i).get(currentWeekIndex)) : "";
                 childView.setTextViewText(R.id.CatAmount, catAmount);
-                childView.setTextViewText(R.id.CatName,(String) output.get(i).get(0));
+                childView.setTextViewText(R.id.CatName,(String) responseData.get(i).get(0));
 
                 newView.addView(R.id.CatsList, childView);
             }
-
+            // add week info to DisplayBar:
+            StringBuilder builder = new StringBuilder();
+            builder.append("week ");
+            builder.append(responseData.get(OFFSETTOP-1).get(currentWeekIndex));
+            builder.append(" ");
+            builder.append(responseData.get(OFFSETTOP+categoryCount+OFFSETBOTTOM - 1).get(currentWeekIndex));
+            newView.setTextViewText(R.id.DisplayBar,builder);
             // update the app widget
             appWidgetManager.updateAppWidget(appWidgetID,newView);
         }
     }
+
+    // This gets called if the user hasn't granted the app perms to their Google Sheets account
+    @Override
+    protected void onCancelled() {
+        mProgress.hide();
+        if (mLastError != null) {
+            if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+//                showGooglePlayServicesAvailabilityErrorDialog(
+//                        ((GooglePlayServicesAvailabilityIOException) mLastError)
+//                                .getConnectionStatusCode());
+            } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                ((Activity) context).startActivityForResult(
+                        ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                        MainActivity.REQUEST_AUTHORIZATION);
+            } else {
+                Log.d(TAG,"The following error occurred:\n"
+                        + mLastError.getMessage());
+            }
+        } else {
+            Log.d(TAG,"Request cancelled.");
+        }
+    }
+
 
     // region Helper Methods
 
@@ -159,18 +199,42 @@ public class InitTask extends AsyncTask<Void, Void, List<List<Object>>> {
         return currentWeekIndex;
     }
 
-    private int getRowCount(List<List<Object>> responseData){
-//        String[] dataStringsArray = dataRangeString.split(":");
-//        String rangeStartString = dataStringsArray[0];
-//        String rangeEndString = dataStringsArray[1];
-//
-//        int startRow = Integer.parseInt(stripNonDigits(rangeStartString));
-//        int endRow = Integer.parseInt(stripNonDigits(rangeEndString));
-//
-//        return endRow - startRow + 1;
+    private int getRowCount(Point[] dataRangePoints){
+        if (dataRangePoints.length < 2) {
+            throw new IllegalArgumentException("Need an array of at least 2 points!");
+        }
 
-        return responseData.size() - 3;
+        return dataRangePoints[1].x - dataRangePoints[0].x + 1;
     }
+
+    private Point[] A12Coords(String A1dataString){
+        String[] dataStringsArray = A1dataString.split(":");
+        String rangeStartString = dataStringsArray[0];
+        String rangeEndString = dataStringsArray[1];
+
+        int startRow = Integer.parseInt(stripNonDigits(rangeStartString));
+        int endRow = Integer.parseInt(stripNonDigits(rangeEndString));
+
+        int startCol = columnLetter2Number(stripNonLetters(rangeStartString));
+        int endCol = columnLetter2Number(stripNonLetters(rangeEndString));
+
+        return new Point[]{new Point(startRow,startCol),new Point(endRow,endCol)};
+    }
+
+    private String Coords2A1(Point[] points) {
+        if (points.length < 2){
+            throw new IllegalArgumentException("Need an array of at least 2 points!");
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(number2ColumnLetter(points[0].y));   // column
+        builder.append(points[0].x);    // row
+        builder.append(":");
+        builder.append(number2ColumnLetter(points[1].y));   // column
+        builder.append(points[1].x);    // row
+
+        return builder.toString();
+    }
+
 
     private String stripNonDigits(final CharSequence input){
         final StringBuilder sb = new StringBuilder(input.length());
@@ -181,6 +245,49 @@ public class InitTask extends AsyncTask<Void, Void, List<List<Object>>> {
             }
         }
         return sb.toString();
+    }
+
+    private String stripNonLetters(final CharSequence input){
+        final StringBuilder sb = new StringBuilder(input.length());
+        for(int i = 0; i < input.length(); i++){
+            final char c = input.charAt(i);
+            if(c > 64 && c < 91){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public int columnLetter2Number(String inputColumnName) {
+        int outputColumnNumber = 0;
+
+        if (inputColumnName == null || inputColumnName.length() == 0) {
+            throw new IllegalArgumentException("Input is not valid!");
+        }
+
+        int i = inputColumnName.length() - 1;
+        int t = 0;
+        while (i >= 0) {
+            char curr = inputColumnName.charAt(i);
+            outputColumnNumber = outputColumnNumber + (int) Math.pow(26, t) * (curr - 'A' + 1);
+            t++;
+            i--;
+        }
+        return outputColumnNumber;
+    }
+
+    public String number2ColumnLetter(int inputColumnNumber) {
+        String outputColumnName = "";
+        int Base = 26;
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        int TempNumber = inputColumnNumber;
+        while (TempNumber > 0) {
+            int position = TempNumber % Base;
+            outputColumnName = (position == 0 ? 'Z' : chars.charAt(position > 0 ? position - 1 : 0)) + outputColumnName;
+            TempNumber = (TempNumber - 1) / Base;
+        }
+        return outputColumnName;
     }
 
     // endregion
